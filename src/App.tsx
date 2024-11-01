48dcad7a1dbcddff396e329a9e39c669fd19ac74
Fran Rubio <frubio1207@gmail.com>
2024-11-01 11:37:21 +0100

    Ajuste Footer TS

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
 
 <footer className="bg-dark mt-5 py-5">
     <div className="container-xl">
-        <p className="text-white text-center fs-4 mt-4 m-md-0">Proyecto Web SurfShop - HTML5, CSS3, Bootstrap, Javascript y React</p>
+        <p className="text-white text-center fs-4 mt-4 m-md-0">Proyecto Web SurfShop - HTML5, CSS3, Bootstrap, TypeScript y React</p>
         <p className="text-white text-center fs-2 mt-4 m-md-0">Este proyecto consiste en la creación de una vista de productos con interacción con el carrito a traves del uso de useState, useEffect, useMemo y customHooks en React. Además se ha usado algunas clases de utilidad de Bootstrap para dar estilo CSS. Adicionalmente se usa el LocalStorage para mantener los artículos en el carrito tras refrescar la página.</p>
         <p className="text-white text-center fs-4 mt-4 m-md-0">Creado por FranRubioDev</p>
 
